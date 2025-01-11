11cda7dac64b5cda7e6ed2b2bb9c78d87c486327
dG9hc3Q <dG9hc3Q@icloud.com>
2025-01-11 13:52:53 +1000

    feat: file tree header

--- src/workspace/file_tree.rs
+++ src/workspace/file_tree.rs
@@ -18,17 +18,19 @@ impl FileTree {
             .selection_mode(gtk::SelectionMode::Single)
             .build();
 
-        /*let label = Label::builder()
+        let label = Label::builder()
             .label("Workspace Open Files")
             .css_classes(vec!["file-tree-header"])
-            .build();*/
+            .height_request(28)
+            .build();
 
         // Create a box to hold both the label and list with consistent background
         let inner_container = GtkBox::builder()
             .orientation(Orientation::Vertical)
             .css_classes(vec!["file-tree-inner"])
             .build();
-        // inner_container.append(&label);
+
+        inner_container.append(&label);
         inner_container.append(&file_list);
 
         // Create the outer container with margin
